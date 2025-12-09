c6818271e12916d73ab0a8b0daf94c2ac4920393
manikanthreddynaredla <manikanthreddynaredla@github.com>
2025-12-09 22:14:24 +0530

    variables program

--- src/main/java/org/example/typesofvariables.java
+++ src/main/java/org/example/typesofvariables.java
@@ -2,5 +2,24 @@ package org.example;
 
 public class typesofvariables
 {
+static String name = "manikanth";
+int age = 12;
+
+void diplay()
+{
+ String nationality = "indian";
+ System.out.println(nationality);
+
+}
+public static void main(String[]args)
+{
+    typesofvariables a = new typesofvariables();
+
+    System.out.println(name);
+    System.out.println(a.age);
+    a.diplay();
+
+
+}
 
 }
