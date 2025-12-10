f1ed2ebe3c6451e4e42be25c09e768b6744a1beb
manikanthreddynaredla <manikanthreddynaredla@github.com>
2025-12-10 15:29:17 +0530

    methods

---
+++ src/main/java/methods/methodwithreturntype.java
@@ -0,0 +1,17 @@
+package methods;
+
+public class methodwithreturntype
+{
+    int squareroot(){
+        int a = 25;
+        System.out.println("Square root of 5");
+        return a;
+    }
+    public static void main(String[] args)
+    {
+        methodwithreturntype a = new methodwithreturntype();
+        System.out.println(a.squareroot());
+
+
+    }
+}
